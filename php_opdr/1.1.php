8985ec6d515752022563d3b9eaf7ebaa16e47830
JavonCroes <9021965@student.zadkine.nl>
2024-09-09 11:43:59 +0200

    opdr 1.1

---
+++ php_opdr/1.1.php
@@ -0,0 +1,8 @@
+<?php
+
+$firstName = "Jan";
+$age = 25;
+$height = 1.75;
+$isStudent = true;
+
+echo "Voornaam: " . $firstName . "<br>" . "Leeftijd: " . $age . "<br>" . "Lengte: " . $height . "<br>" . "Student: " . $isStudent;

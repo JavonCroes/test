10107ac32d66dde036ab9c4765452d4e38168842
JavonCroes <9021965@student.zadkine.nl>
2024-09-09 11:51:21 +0200

    opdr 1.2

---
+++ php_opdr/1.2.php
@@ -0,0 +1,15 @@
+<?php
+
+// Correcte variabelenamen
+$age;
+$firstName;
+$totalAmount;
+$_count;
+$myVariable123;
+
+// Ongeldige variabelenamen 
+$1invalidName;
+$my-variable;
+$@specialChars;
+$function;
+$class;

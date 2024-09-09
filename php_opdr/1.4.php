3ea39e594b9d45ba4b786be7b8522e5cfd6c9435
JavonCroes <9021965@student.zadkine.nl>
2024-09-09 12:01:50 +0200

    opdr 1.4

---
+++ php_opdr/1.4.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 10;
+$b = 3.14;
+$c = "PHP";
+
+$a = "Tien";
+$b = 3;
+$c = false;
+
+echo "Nieuwe waarden:\n";
+echo "a: $a\n";
+echo "b: $b\n";
+echo "c: $c\n";
+
+echo "Var_dump:\n";
+var_dump($a);
+var_dump($b);
+var_dump($c);
